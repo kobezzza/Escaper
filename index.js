7007648ca02853fba0067c2813acc606b4ce3a97
kobezzza <kobezzza@mail.ru>
2013-08-10 12:08:50 +0400

    Первый коммит

---
+++ index.js
@@ -0,0 +1,17 @@
+var __NEJS_THIS__ = this;
+var Program = require('commander');
+
+Program
+	.version('0.0.1')
+	.option('-s, --source [src]', 'source file')
+	.option('-o, --output [src]', 'output file')
+	.option('-c, --comment [src]', 'with comment')
+	.parse(process.argv);
+
+var fs = require('fs');
+fs.writeFileSync(
+	Program.output,
+	require('./escaper').replace(
+		String(fs.readFileSync(Program.source), !!Program.comment)
+	)
+);
