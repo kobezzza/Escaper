47cfd0f11c4b4937b90fad84c8e9124b9ef29d1a
Kobezzza <kobezzza@mail.ru>
2014-06-12 13:12:17 +0400

    Добавлен скрипт для сброса кеша WS

---
+++ reset-ide-cache.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var path = require('path');
+
+(function exec(dirname) {
+	fs.readdirSync(dirname).forEach(function(el)  {
+		var src = path.join(dirname, el);
+		var stats = fs.statSync(src);
+
+		if (stats.isDirectory()) {
+			exec(src);
+
+		} else if (stats.isFile()) {
+			var ext = path.extname(el);
+
+			if (ext === '.jsn') {
+				var file = fs.readFileSync(src);
+
+				fs.writeFileSync(src, file + ' ');
+				fs.writeFileSync(src, file);
+			}
+		}
+	});
+})(__dirname);
