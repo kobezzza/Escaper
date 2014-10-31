e72ec0d4133ba97146698427262d2171b71afa65
Kobezzza <kobezzza@mail.ru>
2014-10-31 09:53:57 +0400

    Обновление

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var es6 = require('gulp-es6-transpiler'),
 	concat = require('gulp-concat'),
 	bump = require('gulp-bump');
 
-gulp.task('es6-transpiler', function () {
+gulp.task('build', function () {
 	gulp.src('./lib/*.js')
 		.pipe(concat('escaper.js'))
 		.pipe(es6({disallowUnknownReferences: false}))
@@ -20,7 +20,7 @@ gulp.task('bump', function () {
 });
 
 gulp.task('watch', function () {
-	gulp.watch('./lib/*.js', ['es6-transpiler', 'bump']);
+	gulp.watch('./lib/*.js', ['build', 'bump']);
 });
 
-gulp.task('default', ['es6-transpiler', 'bump', 'watch']);
+gulp.task('default', ['build', 'bump']);
