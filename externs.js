2d08ff033d4fd654bcea81e72dec2ab0323126ae
kobezzza <kobezzza@gmail.com>
2014-12-15 12:20:54 +0400

    Добавлен файл экстернов

---
+++ externs.js
@@ -0,0 +1,33 @@
+/** @type {!Object} */
+var Escaper = {
+
+	/** @type {!Array} */
+	VERSION: [],
+
+	/** @type {!Array} */
+	quotContent: [],
+
+	/** @type {RegExp} */
+	snakeskinRgxp: null,
+
+	/** @type {?string} */
+	symbols: null,
+
+	/**
+	 * @abstract
+	 * @param {string} str
+	 * @param {(Object|boolean)=} [opt_withCommentsOrParams]
+	 * @param {Array=} [opt_quotContent]
+	 * @param {?boolean=} [opt_snakeskin]
+	 * @return {string}
+	 */
+	replace: function (str, opt_withCommentsOrParams, opt_quotContent, opt_snakeskin) {},
+
+	/**
+	 * @abstract
+	 * @param {string} str
+	 * @param {Array=} [opt_quotContent]
+	 * @return {string}
+	 */
+	paste: function (str, opt_quotContent) {}
+};
