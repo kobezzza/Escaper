37bc4df5d0db0b2a755ccd7f8613ccd726cf4d1f
kobezzza <kobezzza@gmail.com>
2015-02-02 14:54:12 +0300

    Update

--- dist/escaper.js
+++ dist/escaper.js
@@ -1,135 +1,135 @@
 /*!
- * Escaper v2.1.11
+ * Escaper v2.1.13
  * https://github.com/kobezzza/Escaper
  *
  * Released under the MIT license
  * https://github.com/kobezzza/Escaper/blob/master/LICENSE
  *
- * Date: Tue, 27 Jan 2015 09:26:53 GMT
+ * Date: Mon, 02 Feb 2015 11:33:12 GMT
  */
 
 (function () {
-"use strict";
-
+'use strict';
+var self = this;
 var Escaper = {
-  VERSION: [2, 1, 12]
+	VERSION: [2, 1, 13]
 };
 
 if (typeof define === "function" && define.amd) {
-  define([], function () {
-    return Escaper;
-  });
-} else if (typeof exports === "object") {
-  module.exports = exports = Escaper;
+	define([], function () {
+		return Escaper;
+	});
+} else if (typeof module === "object" && typeof exports === "object") {
+	module.exports = exports = Escaper;
 } else {
-  this.Escaper = Escaper;
+	self.Escaper = Escaper;
 }
 
 var stringLiterals = {
-  "\"": true,
-  "'": true,
-  "`": true
+	"\"": true,
+	"'": true,
+	"`": true
 };
 
 var literals = {
-  "/": true
+	"/": true
 };
 
 for (var key in stringLiterals) {
-  if (!stringLiterals.hasOwnProperty(key)) {
-    continue;
-  }
+	if (!stringLiterals.hasOwnProperty(key)) {
+		continue;
+	}
 
-  literals[key] = true;
+	literals[key] = true;
 }
 
 var singleComments = {
-  "//": true
+	"//": true
 };
 
 var multComments = {
-  "/*": true,
-  "/**": true,
-  "/*!": true
+	"/*": true,
+	"/**": true,
+	"/*!": true
 };
 
 var keyArr = [],
     finalMap = {};
 
 for (var key in literals) {
-  if (!literals.hasOwnProperty(key)) {
-    continue;
-  }
+	if (!literals.hasOwnProperty(key)) {
+		continue;
+	}
 
-  keyArr.push(key);
-  finalMap[key] = true;
+	keyArr.push(key);
+	finalMap[key] = true;
 }
 
 for (var key in singleComments) {
-  if (!singleComments.hasOwnProperty(key)) {
-    continue;
-  }
+	if (!singleComments.hasOwnProperty(key)) {
+		continue;
+	}
 
-  keyArr.push(key);
-  finalMap[key] = true;
+	keyArr.push(key);
+	finalMap[key] = true;
 }
 
 for (var key in multComments) {
-  if (!multComments.hasOwnProperty(key)) {
-    continue;
-  }
+	if (!multComments.hasOwnProperty(key)) {
+		continue;
+	}
 
-  keyArr.push(key);
-  finalMap[key] = true;
+	keyArr.push(key);
+	finalMap[key] = true;
 }
 
 var rgxpFlagsMap = {
-  "g": true,
-  "m": true,
-  "i": true,
-  "y": true,
-  "u": true
+	"g": true,
+	"m": true,
+	"i": true,
+	"y": true,
+	"u": true
 };
 
 var rgxpFlags = [];
 for (var key in rgxpFlagsMap) {
-  if (!rgxpFlagsMap.hasOwnProperty(key)) {
-    continue;
-  }
+	if (!rgxpFlagsMap.hasOwnProperty(key)) {
+		continue;
+	}
 
-  rgxpFlags.push(key);
+	rgxpFlags.push(key);
 }
 
 var escapeEndMap = {
-  "-": true,
-  "+": true,
-  "*": true,
-  "%": true,
-  "~": true,
-  ">": true,
-  "<": true,
-  "^": true,
-  ",": true,
-  ";": true,
-  "=": true,
-  "|": true,
-  "&": true,
-  "!": true,
-  "?": true,
-  ":": true,
-  "(": true,
-  "{": true,
-  "[": true
+	"-": true,
+	"+": true,
+	"*": true,
+	"%": true,
+	"~": true,
+	">": true,
+	"<": true,
+	"^": true,
+	",": true,
+	";": true,
+	"=": true,
+	"|": true,
+	"&": true,
+	"!": true,
+	"?": true,
+	":": true,
+	"(": true,
+	"{": true,
+	"[": true
 };
 
 var escapeEndWordMap = {
-  "typeof": true,
-  "void": true,
-  "instanceof": true,
-  "delete": true,
-  "in": true,
-  "new": true,
-  "of": true
+	"typeof": true,
+	"void": true,
+	"instanceof": true,
+	"delete": true,
+	"in": true,
+	"new": true,
+	"of": true
 };
 
 var cache = {},
@@ -141,15 +141,15 @@ var cache = {},
  * @param {(boolean|number)} val
  */
 function mix(obj, p, val) {
-  for (var key in obj) {
-    if (!obj.hasOwnProperty(key)) {
-      continue;
-    }
+	for (var key in obj) {
+		if (!obj.hasOwnProperty(key)) {
+			continue;
+		}
 
-    if (key in p === false) {
-      p[key] = val;
-    }
-  }
+		if (key in p === false) {
+			p[key] = val;
+		}
+	}
 }
 
 /** @type {!Array} */
@@ -166,14 +166,14 @@ Escaper.symbols = null;
 Escaper.snakeskinRgxp = null;
 
 /**
- * Replace all found blocks ' ... ', " ... ", ` ... `, / ... /, // ..., /* ... *\/ on
+ * Replace all found blocks ' ... ', " ... ", ` ... `, / ... /, // ..., /* ... *\/ to
  * __ESCAPER_QUOT__number_ in a specified string
  *
  * @param {string} str - source string
  * @param {(Object.<string, boolean>|boolean)=} [opt_withCommentsOrParams=false] - parameters:
  *
- *     (if a value of parameter will be set to -1, then all found matches will be removed from a final string,
- *          or if the value will set to true/false they will be included/excluded)
+ *     (if a value of parameter is set to -1, then all found matches will be removed from a final string,
+ *          or if the value will be set to true/false they will be included/excluded)
  *
  *     *) @all      - replaces all matches
  *     *) @comments - replaces all kinds of comments
@@ -195,234 +195,234 @@ Escaper.snakeskinRgxp = null;
  * @return {string}
  */
 Escaper.replace = function (str, opt_withCommentsOrParams, opt_quotContent, opt_snakeskin) {
-  symbols = symbols || Escaper.symbols || "a-z";
+	symbols = symbols || Escaper.symbols || "a-z";
 
-  snakeskinRgxp = snakeskinRgxp || Escaper.snakeskinRgxp || new RegExp("[!$" + symbols + "_]", "i");
+	snakeskinRgxp = snakeskinRgxp || Escaper.snakeskinRgxp || new RegExp("[!$" + symbols + "_]", "i");
 
-  var isObj = opt_withCommentsOrParams instanceof Object;
-  var p = isObj ? Object(opt_withCommentsOrParams) : {};
+	var isObj = opt_withCommentsOrParams instanceof Object;
+	var p = isObj ? Object(opt_withCommentsOrParams) : {};
 
-  var withComments = false;
-  if (typeof opt_withCommentsOrParams === "boolean") {
-    withComments = Boolean(opt_withCommentsOrParams);
-  }
+	var withComments = false;
+	if (typeof opt_withCommentsOrParams === "boolean") {
+		withComments = Boolean(opt_withCommentsOrParams);
+	}
 
-  if ("@comments" in p) {
-    mix(multComments, p, p["@comments"]);
-    mix(singleComments, p, p["@comments"]);
-    delete p["@comments"];
-  }
+	if ("@comments" in p) {
+		mix(multComments, p, p["@comments"]);
+		mix(singleComments, p, p["@comments"]);
+		delete p["@comments"];
+	}
 
-  if ("@strings" in p) {
-    mix(stringLiterals, p, p["@strings"]);
-    delete p["@strings"];
-  }
+	if ("@strings" in p) {
+		mix(stringLiterals, p, p["@strings"]);
+		delete p["@strings"];
+	}
 
-  if ("@literals" in p) {
-    mix(literals, p, p["@literals"]);
-    delete p["@literals"];
-  }
+	if ("@literals" in p) {
+		mix(literals, p, p["@literals"]);
+		delete p["@literals"];
+	}
 
-  if ("@all" in p) {
-    mix(finalMap, p, p["@all"]);
-    delete p["@all"];
-  }
+	if ("@all" in p) {
+		mix(finalMap, p, p["@all"]);
+		delete p["@all"];
+	}
 
-  var cacheKey = "";
-  for (var i = -1; ++i < keyArr.length;) {
-    var el = keyArr[i];
+	var cacheKey = "";
+	for (var i = -1; ++i < keyArr.length;) {
+		var el = keyArr[i];
 
-    if (multComments[el] || singleComments[el]) {
-      p[el] = withComments || p[el];
-    } else {
-      p[el] = p[el] || !isObj;
-    }
+		if (multComments[el] || singleComments[el]) {
+			p[el] = withComments || p[el];
+		} else {
+			p[el] = p[el] || !isObj;
+		}
 
-    cacheKey += "" + p[el] + ",";
-  }
+		cacheKey += "" + p[el] + ",";
+	}
 
-  var initStr = str;
-  var stack = opt_quotContent || content;
+	var initStr = str;
+	var stack = opt_quotContent || content;
 
-  if (stack === content && cache[cacheKey] && cache[cacheKey][initStr]) {
-    return cache[cacheKey][initStr];
-  }
+	if (stack === content && cache[cacheKey] && cache[cacheKey][initStr]) {
+		return cache[cacheKey][initStr];
+	}
 
-  var begin = false,
-      end = true;
+	var begin = false,
+	    end = true;
 
-  var escape = false,
-      comment = false;
+	var escape = false,
+	    comment = false;
 
-  var selectionStart = 0,
-      block = false;
+	var selectionStart = 0,
+	    block = false;
 
-  var templateVar = 0,
-      filterStart = false;
+	var templateVar = 0,
+	    filterStart = false;
 
-  var cut, label;
+	var cut, label;
 
-  var part = "",
-      rPart = "";
+	var part = "",
+	    rPart = "";
 
-  for (var i = -1; ++i < str.length;) {
-    var el = str.charAt(i),
-        next = str.charAt(i + 1);
+	for (var i = -1; ++i < str.length;) {
+		var el = str.charAt(i),
+		    next = str.charAt(i + 1);
 
-    var word = str.substr(i, 2),
-        extWord = str.substr(i, 3);
+		var word = str.substr(i, 2),
+		    extWord = str.substr(i, 3);
 
-    if (!comment) {
-      if (!begin) {
-        if (el === "/") {
-          if (singleComments[word] || multComments[word]) {
-            if (singleComments[extWord] || multComments[extWord]) {
-              comment = extWord;
-            } else {
-              comment = word;
-            }
-          }
+		if (!comment) {
+			if (!begin) {
+				if (el === "/") {
+					if (singleComments[word] || multComments[word]) {
+						if (singleComments[extWord] || multComments[extWord]) {
+							comment = extWord;
+						} else {
+							comment = word;
+						}
+					}
 
-          if (comment) {
-            selectionStart = i;
-            continue;
-          }
-        }
+					if (comment) {
+						selectionStart = i;
+						continue;
+					}
+				}
 
-        if (escapeEndMap[el] || escapeEndWordMap[rPart]) {
-          end = true;
-          rPart = "";
-        } else if (uSRgxp.test(el)) {
-          end = false;
-        }
+				if (escapeEndMap[el] || escapeEndWordMap[rPart]) {
+					end = true;
+					rPart = "";
+				} else if (uSRgxp.test(el)) {
+					end = false;
+				}
 
-        if (wRgxp.test(el)) {
-          part += el;
-        } else {
-          rPart = part;
-          part = "";
-        }
+				if (wRgxp.test(el)) {
+					part += el;
+				} else {
+					rPart = part;
+					part = "";
+				}
 
-        var skip = false;
-        if (opt_snakeskin) {
-          if (el === "|" && snakeskinRgxp.test(next)) {
-            filterStart = true;
-            end = false;
-            skip = true;
-          } else if (filterStart && sRgxp.test(el)) {
-            filterStart = false;
-            end = true;
-            skip = true;
-          }
-        }
+				var skip = false;
+				if (opt_snakeskin) {
+					if (el === "|" && snakeskinRgxp.test(next)) {
+						filterStart = true;
+						end = false;
+						skip = true;
+					} else if (filterStart && sRgxp.test(el)) {
+						filterStart = false;
+						end = true;
+						skip = true;
+					}
+				}
 
-        if (!skip) {
-          if (escapeEndMap[el]) {
-            end = true;
-          } else if (uSRgxp.test(el)) {
-            end = false;
-          }
-        }
-      }
+				if (!skip) {
+					if (escapeEndMap[el]) {
+						end = true;
+					} else if (uSRgxp.test(el)) {
+						end = false;
+					}
+				}
+			}
 
-      // [] inside RegExp
-      if (begin === "/" && !escape) {
-        if (el === "[") {
-          block = true;
-        } else if (el === "]") {
-          block = false;
-        }
-      }
+			// [] inside RegExp
+			if (begin === "/" && !escape) {
+				if (el === "[") {
+					block = true;
+				} else if (el === "]") {
+					block = false;
+				}
+			}
 
-      if (!begin && templateVar) {
-        if (el === "}") {
-          templateVar--;
-        } else if (el === "{") {
-          templateVar++;
-        }
+			if (!begin && templateVar) {
+				if (el === "}") {
+					templateVar--;
+				} else if (el === "{") {
+					templateVar++;
+				}
 
-        if (!templateVar) {
-          el = "`";
-        }
-      }
+				if (!templateVar) {
+					el = "`";
+				}
+			}
 
-      if (begin === "`" && !escape && word === "${") {
-        el = "`";
-        i++;
-        templateVar++;
-      }
+			if (begin === "`" && !escape && word === "${") {
+				el = "`";
+				i++;
+				templateVar++;
+			}
 
-      if (finalMap[el] && (el !== "/" || end) && !begin) {
-        begin = el;
-        selectionStart = i;
-      } else if (begin && (el === "\\" || escape)) {
-        escape = !escape;
-      } else if (finalMap[el] && begin === el && !escape && (begin !== "/" || !block)) {
-        if (el === "/") {
-          for (var j = -1; ++j < rgxpFlags.length;) {
-            if (rgxpFlagsMap[str.charAt(i + 1)]) {
-              i++;
-            }
-          }
-        }
+			if (finalMap[el] && (el !== "/" || end) && !begin) {
+				begin = el;
+				selectionStart = i;
+			} else if (begin && (el === "\\" || escape)) {
+				escape = !escape;
+			} else if (finalMap[el] && begin === el && !escape && (begin !== "/" || !block)) {
+				if (el === "/") {
+					for (var j = -1; ++j < rgxpFlags.length;) {
+						if (rgxpFlagsMap[str.charAt(i + 1)]) {
+							i++;
+						}
+					}
+				}
 
-        begin = false;
-        end = false;
+				begin = false;
+				end = false;
 
-        if (p[el]) {
-          cut = str.substring(selectionStart, i + 1);
+				if (p[el]) {
+					cut = str.substring(selectionStart, i + 1);
 
-          if (p[el] === -1) {
-            label = "";
-          } else {
-            label = "__ESCAPER_QUOT__" + stack.length + "_";
-            stack.push(cut);
-          }
+					if (p[el] === -1) {
+						label = "";
+					} else {
+						label = "__ESCAPER_QUOT__" + stack.length + "_";
+						stack.push(cut);
+					}
 
-          str = str.substring(0, selectionStart) + label + str.substring(i + 1);
-          i += label.length - cut.length;
-        }
-      }
-    } else if (nRgxp.test(next) && singleComments[comment] || multComments[el + str.charAt(i - 1)] && i - selectionStart > 2 && multComments[comment]) {
-      if (p[comment]) {
-        cut = str.substring(selectionStart, i + 1);
+					str = str.substring(0, selectionStart) + label + str.substring(i + 1);
+					i += label.length - cut.length;
+				}
+			}
+		} else if (nRgxp.test(next) && singleComments[comment] || multComments[el + str.charAt(i - 1)] && i - selectionStart > 2 && multComments[comment]) {
+			if (p[comment]) {
+				cut = str.substring(selectionStart, i + 1);
 
-        if (p[comment] === -1) {
-          label = "";
-        } else {
-          label = "__ESCAPER_QUOT__" + stack.length + "_";
-          stack.push(cut);
-        }
+				if (p[comment] === -1) {
+					label = "";
+				} else {
+					label = "__ESCAPER_QUOT__" + stack.length + "_";
+					stack.push(cut);
+				}
 
-        str = str.substring(0, selectionStart) + label + str.substring(i + 1);
-        i += label.length - cut.length;
-      }
+				str = str.substring(0, selectionStart) + label + str.substring(i + 1);
+				i += label.length - cut.length;
+			}
 
-      comment = false;
-    }
-  }
+			comment = false;
+		}
+	}
 
-  if (stack === content) {
-    cache[cacheKey] = cache[cacheKey] || {};
-    cache[cacheKey][initStr] = str;
-  }
+	if (stack === content) {
+		cache[cacheKey] = cache[cacheKey] || {};
+		cache[cacheKey][initStr] = str;
+	}
 
-  return str;
+	return str;
 };
 
 var pasteRgxp = /__ESCAPER_QUOT__(\d+)_/g;
 
 /**
- * Replace all found blocks __ESCAPER_QUOT__number_ on real content in a specified string
+ * Replace all found blocks __ESCAPER_QUOT__number_ to real content in a specified string
  *
  * @param {string} str - source string
  * @param {Array=} [opt_quotContent=Escaper.quotContent] - stack of content
  * @return {string}
  */
 Escaper.paste = function (str, opt_quotContent) {
-  var stack = opt_quotContent || content;
-  return str.replace(pasteRgxp, function (sstr, pos) {
-    return stack[pos];
-  });
+	var stack = opt_quotContent || content;
+	return str.replace(pasteRgxp, function (sstr, pos) {
+		return stack[pos];
+	});
 };
 }).call(new Function('return this')());
