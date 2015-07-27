26dd75677fa898f151f8251374a8683df71d76ee
kobezzza <kobezzza@gmail.com>
2015-07-27 09:49:46 +0300

    :arrow_up: @2.4.10

--- src/escaper.js
+++ src/escaper.js
@@ -6,7 +6,7 @@
  * https://github.com/kobezzza/Escaper/blob/master/LICENSE
  */
 
-const Escaper = { VERSION: [2, 4, 9] };
+const Escaper = { VERSION: [2, 4, 10] };
 export default Escaper;
 
 const stringLiterals = {
