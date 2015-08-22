30e6fa5ac21d88af69bb85f414d24fe80b45321f
kobezzza <kobezzza@gmail.com>
2015-08-22 17:09:53 +0300

    Externs update

--- externs.js
+++ externs.js
@@ -11,8 +11,11 @@ var Escaper = {
 	/** @type {!Array} */
 	VERSION: [],
 
+	/** @type {!Object} */
+	cache: [],
+
 	/** @type {!Array} */
-	quotContent: [],
+	content: [],
 
 	/** @type {?string} */
 	symbols: null,
@@ -24,17 +27,17 @@ var Escaper = {
 	 * @abstract
 	 * @param {string} str
 	 * @param {(Object<string, boolean>|boolean)=} [opt_withCommentsOrParams]
-	 * @param {Array=} [opt_quotContent]
+	 * @param {Array=} [opt_content]
 	 * @param {?boolean=} [opt_snakeskin]
 	 * @return {string}
 	 */
-	replace: function (str, opt_withCommentsOrParams, opt_quotContent, opt_snakeskin) {},
+	replace: function (str, opt_withCommentsOrParams, opt_content, opt_snakeskin) {},
 
 	/**
 	 * @abstract
 	 * @param {string} str
-	 * @param {Array=} [opt_quotContent]
+	 * @param {Array=} [opt_content]
 	 * @return {string}
 	 */
-	paste: function (str, opt_quotContent) {}
+	paste: function (str, opt_content) {}
 };
